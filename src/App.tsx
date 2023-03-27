b618bf15c7a8ad7d119f1d0de85461acb0a3b0ef
Blake Bollinger <blake@blakebollinger.dev>
2023-03-27 10:39:28 -0500

    Added basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,76 @@
 import React from 'react';
 import './App.css';
-import { Text } from '@chakra-ui/react';
+import {
+	Text,
+	Textarea,
+	Flex,
+	Table,
+	TableContainer,
+	TableCaption,
+	Thead,
+	Tr,
+	Th,
+	Tbody,
+	Td,
+	Spacer,
+    VStack
+} from '@chakra-ui/react';
 
 function App() {
 	return (
 		<div className="App">
 			<Text>Welcome to my Shift Reduce Parser!</Text>
+
+			<Flex p={'5%'}>
+
+				<VStack w={'40%'}>
+					<Text fontSize={'lg'}>Input Grammar</Text>
+					<Textarea resize={'none'}/>
+				</VStack>
+
+				<Spacer />
+
+				<VStack w={'40%'}>
+					<Text fontSize={'lg'}>Parser Table</Text>
+
+					<TableContainer >
+					<Table variant='simple' size={'sm'}>
+						<TableCaption>Generated from the Input Grammar</TableCaption>
+						<Thead>
+							<Tr>
+								<Th>Action</Th>
+								<Th></Th>
+								<Th>Goto</Th>
+							</Tr>
+						</Thead>
+						<Tbody>
+							<Tr>
+								<Td>State</Td>
+								<Td></Td>
+								<Td></Td>
+							</Tr>
+							<Tr>
+								<Td>0</Td>
+								<Td></Td>
+								<Td ></Td>
+							</Tr>
+							<Tr>
+								<Td>1</Td>
+								<Td></Td>
+								<Td></Td>
+							</Tr>
+						</Tbody>
+					</Table>
+				</TableContainer>
+				</VStack>
+
+			</Flex>
+
+			<VStack w={'40%'} m={'auto'}>
+				<Text fontSize={'lg'}>Parsing Steps</Text>
+				<Textarea isDisabled resize={'none'}/>
+			</VStack>
+
 		</div>
 	);
 }
